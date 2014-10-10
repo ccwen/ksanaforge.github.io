efc804e79e0932b68e2d9a92647b5b4bf3a7947c
yapcheahshen <yapcheahshen@gmail.com>
2014-10-10 17:38:28 +0800

    change back to english

--- pncdemo/build.js
+++ pncdemo/build.js
@@ -15536,9 +15536,9 @@ require.register("pncdemo-markuppanel/tagset_news.js", function(exports, require
 module.exports=[
    {caption:"glyphicon-eye-open",tag:null,dialog: Require("markoverview")} //nothing happen
   ,{caption:"Why", tag:"why", dialog: Require("marksimple")  } 
-  ,{caption:"地", tag:"where", dialog: Require("marksimple")  } 
+  ,{caption:"Where", tag:"where", dialog: Require("marksimple")  } 
   ,{caption:"What", tag:"what", dialog: Require("marksimple")  } 
-  ,{caption:"時", tag:"when",dialog:Require("marksimple")}  
+  ,{caption:"When", tag:"when",dialog:Require("marksimple")}  
   ,{caption:"Who", tag:"who",dialog:Require("marksimple")} 
   ,{caption:"How",tag:"how",dialog:Require("marksimple")} 
   ,{caption:"glyphicon-comment", tag:"footnote",dialog:Require("markfootnote")}  //single view dialog markup
